81cab8518152ad0a6be3ee05c8194c2f1d1cb8ca
Gabriele Semeraro <gab.semeraro@gmail.com>
2022-03-24 13:14:34 +0100

    🎨 Added function for checking file extension

---
+++ src/ft_file_checker.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_file_checker.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsemerar <gsemerar@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 13:07:16 by gsemerar          #+#    #+#             */
+/*   Updated: 2022/03/24 13:08:41 by gsemerar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+unsigned int	ft_check_extension(char *file_name, char *ext)
+{
+	char	*check;
+	int		dim_file_name;
+	int		dim_ext;
+
+	dim_file_name = ft_strlen(file_name);
+	dim_ext = ft_strlen(EXT_BER);
+	if (dim_file_name < dim_ext)
+		return (0);
+	check = ft_strnstr(file_name + (dim_file_name - dim_ext), ext, dim_ext);
+	if (!check)
+		return (0);
+	return (1);
+}
