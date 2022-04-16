0f7f31e5419a36387164b8203fb8e5d6159cf473
Gabriele Semeraro <gab.semeraro@gmail.com>
2022-04-16 17:06:57 +0200

    🎨 Update the function for exit in a clear way

--- src/ft_game.c
+++ src/ft_game.c
@@ -6,17 +6,18 @@
 /*   By: gsemerar <gsemerar@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 15:20:19 by gsemerar          #+#    #+#             */
-/*   Updated: 2022/04/15 16:13:39 by gsemerar         ###   ########.fr       */
+/*   Updated: 2022/04/16 16:55:36 by gsemerar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/so_long.h"
 
-static void	ft_check_map(t_map *map);
+static void	ft_check_map(int err, t_map *map);
 
 void	ft_set_map(t_map **map, char *map_file)
 {
 	t_map	*m;
+	int		err;
 
 	m = *map;
 	ft_init_map(m, map_file);
@@ -24,23 +25,25 @@ void	ft_set_map(t_map **map, char *map_file)
 		ft_create_map(m);
 	else
 		ft_perror_exit(ERR_MSG_FILE, ERR_FILE);
-	ft_check_map(m);
+	err = ft_set_error(m);
+	ft_check_map(err, m);
 }
 
-static void	ft_check_map(t_map *map)
+static void	ft_check_map(int err, t_map *map)
 {
-	if (!ft_check_extension(map->file, EXT_BER))
-		ft_perror_exit(ERR_MSG_EXT, ERR_EXT);
-	if (!ft_check_cols_map(map))
-		ft_perror_exit(ERR_MSG_COLS, ERR_COLS);
-	if (!ft_check_size_map(map))
-		ft_perror_exit(ERR_MSG_SIZE, ERR_SIZE);
-	if (!ft_check_perimeter_map(map))
-		ft_perror_exit(ERR_MSG_PERIMETER, ERR_PERIMETER);
-	if (!ft_check_element_type_map(map))
-		ft_perror_exit(ERR_MSG_ELEMENT_TYPE, ERR_ELEMENT_TYPE);
-	if (!ft_check_n_of_element_map(map, PLAYER, 1, 1)
-		|| !ft_check_n_of_element_map(map, COLLECTIBLE, 1, -1)
-		|| !ft_check_n_of_element_map(map, EXIT_MAP, 1, -1))
-		ft_perror_exit(ERR_MSG_ELEMENT_NUM, ERR_ELEMENT_NUM);
+	if (!err)
+		return ;
+	ft_free_map(map);
+	if (err == ERR_EXT)
+		ft_perror_exit(ERR_MSG_EXT, err);
+	if (err == ERR_COLS)
+		ft_perror_exit(ERR_MSG_COLS, err);
+	if (err == ERR_SIZE)
+		ft_perror_exit(ERR_MSG_SIZE, err);
+	if (err == ERR_PERIMETER)
+		ft_perror_exit(ERR_MSG_PERIMETER, err);
+	if (err == ERR_ELEMENT_TYPE)
+		ft_perror_exit(ERR_MSG_ELEMENT_TYPE, err);
+	if (err == ERR_ELEMENT_NUM)
+		ft_perror_exit(ERR_MSG_ELEMENT_NUM, err);
 }
